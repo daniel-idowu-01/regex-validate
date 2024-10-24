c360221ab6196db1a2066aa55703ae3777e4fd05
daniel-idowu-01 <danielidowu414@gmail.com>
2024-10-24 20:08:17 +0100

    add: more secutiry to alphanumeric

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+import { randomBytes } from "crypto";
+
+const alphabetRegex = /^[a-zA-Z]$/;
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+const phoneNumberRegex =
+  /^(?:\+\d{1,3}[-. ]?)?(?:\(\d{1,3}\)[-. ]?)?\d{1,4}[-. ]?\d{1,4}[-. ]?\d{1,9}$/;
+
+const passwordRegex =
+  /^(?=.*[0-9])(?=.*[!@#$%^&*?_\-.,?!:;'“”()\[\]{}+\-×*÷\/=$&*@#%^_|/<>=])[a-zA-Z0-9!@#$%^&*?_\-.,?!:;'“”()\[\]{}+\-×*÷\/=$&*@#%^_|/<>=]{8,30}$/;
+
+// Utility function to generate secure random bytes
+export function secureRandomBytes(length: number): string {
+  return randomBytes(length).toString("hex").slice(0, length);
+}
+
 export function generateRandomNumber(length: number): number {
   if (length < 4) {
     throw new Error("Length must be greater or equal to 4");
@@ -11,13 +28,13 @@ export function generateRandomNumber(length: number): number {
   return randomNum;
 }
 
-export function generateAlphanumericWord(length: number): string {
+export function generateAlphanumericWord(
+  length: number,
+  characters: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+): string {
   if (length < 4) {
     throw new Error("Length must be greater or equal to 4");
   }
-
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
 
   for (let i = 0; i < length; i++) {
@@ -28,27 +45,35 @@ export function generateAlphanumericWord(length: number): string {
   return result;
 }
 
-export function generateAlphanumericWithSymbols(length: number): string {
+export function generateAlphanumericWithSymbols(
+  length: number,
+  characters: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?"
+): string {
+  let newResult;
   if (length < 4) {
     throw new Error("Length must be greater or equal to 4");
   }
 
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
-  let result = "";
-
+  const symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+  const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   // Ensure at least one symbol is included
-  result += characters[Math.floor(Math.random() * 32) + 62];
-  length--;
+  let result = symbols[Math.floor(Math.random() * symbols.length)];
 
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
     result += characters[randomIndex];
   }
 
-  const reverseString = result.split("").reverse().join("");
+  if (!alphabetRegex.test(result[0])) {
+    newResult =
+      alphabets[Math.floor(Math.random() * alphabets.length)] + result;
+  } else {
+    newResult = result;
+  }
 
-  return reverseString;
+  console.log(alphabetRegex.test(result[0]));
+
+  return newResult;
 }
 
 export function generateUsername(firstName: string, lastName: string): string {
@@ -98,11 +123,11 @@ export function generatePassword(difficulty: string): string {
   let password = "";
 
   if (difficulty !== "easy") {
-    password += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[Math.floor(Math.random() * 26)]; 
+    password += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[Math.floor(Math.random() * 26)];
     password += "0123456789"[Math.floor(Math.random() * 10)];
     password += "!@#$%^&*()_+-=[]{}|;:,.<>?"[Math.floor(Math.random() * 32)];
   }
-  
+
   for (let i = password.length; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
     password += characters[randomIndex];
@@ -117,15 +142,23 @@ export function generatePassword(difficulty: string): string {
   return password;
 }
 
+export function isEmail(email: string): boolean {
+  const isEmail = emailRegex.test(email);
+
+  return isEmail;
+}
+
+export function isValidPassword(password: string): boolean {
+  const isValidPassword = passwordRegex.test(password);
+
+  return isValidPassword;
+}
+
 // Example usage
 try {
-  const easyPassword = generatePassword("easy");
-  const mediumPassword = generatePassword("medium");
-  const hardPassword = generatePassword("hard");
+  const easyPassword = generateAlphanumericWithSymbols(30);
 
-  console.log(`Easy Password: ${easyPassword}`);
-  console.log(`Medium Password: ${mediumPassword}`);
-  console.log(`Hard Password: ${hardPassword}`);
+  console.log(`Easy Password11: ${easyPassword}`);
 } catch (error: any) {
   console.error(error.message);
 }
